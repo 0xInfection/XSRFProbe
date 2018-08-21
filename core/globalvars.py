296dd7c64363e65ea174cd389b387adce254625a
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-21 06:37:55 +0100

    Added some new global variables

---
+++ core/globalvars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+#coding: utf-8
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+#Author: the-Infected-Drake (@_tID)
+#This module requires XSRF-Probe
+#https://github.com/the-Infected-Drake/XSRF-Probe
+
+# some global variables needed
+global web
+global form1
+global form2
+global action
+global crawler
+global form
+global init1
+global request
+global Form_Debugger
+
