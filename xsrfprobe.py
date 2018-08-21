4baa5c90c4757945cfa9615822fbac7b4ffedb10
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-21 06:34:11 +0100

    Added raw support for post requests

---
+++ xsrfprobe.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: @_tID
+# This module requires XSRFProbe
+# https://github.com/the-Infected-Drake/XSRFProbe
+
+from core.impo import * # import all stuff
+from core.globalvars import * # import the global variables
+xsrf_main() # the true start of the program ;)
+
+
