c79b9f6b470a4bfaa436523c7f3c68d09f112c06
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-21 06:36:04 +0100

    Added the colours file revised

---
+++ core/colors.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+#Author: @_tID
+#This module requires XSRFProbe
+#https://github.com/the-Infected-Drake/XSRFProbe
+
+###############################
+class color:
+	END  = '\033[0m'  # normal
+	BOLD  = '\033[1m'  # bold
+	RED  = '\033[1;91m' # red
+	GREEN  = '\033[1;92m' # green
+	ORANGE  = '\033[1;93m' # orange
+	BLUE  = '\033[1;94m' # blue
+	PURPLE  = '\033[1;95m' # purple
+	CYAN  = '\033[1;96m' # cyan
+	GREY = '\033[1;97m' # gray
+	TAN  = '\033[1;93m' # tan
+	r3d  = '\033[91m' # red
+	org  = '\033[93m' # orange
+
+O  = '\033[1m \033[93m[!]\033[0m ' # information
+R  = '\033[1m \033[91m[-]\033[0m ' # something's not right
+GR = '\033[1m \033[97m[*]\033[0m ' # processing
+G  = '\033[1m \033[92m[+]\033[0m ' # yay!
+C  = '\033[1m \033[96m[+]\033[0m ' # crawling...
+###############################
+
