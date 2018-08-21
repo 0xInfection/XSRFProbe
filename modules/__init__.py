0da3f35b874001f34397b063ec4fa0c4efbf6ea2
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-21 06:40:23 +0100

    Addition of new init feature

---
+++ modules/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: @_tID
+#This module requires XSRFProbe
+#https://github.com/the-Infected-Drake/XSRFProbe
+
+pass
