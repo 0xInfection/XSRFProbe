092ccf12b53818b409ee0c0e273857c9c3d9ca03
theinfecteddrake <theinfecteddrake@gmail.com>
2018-08-21 06:39:06 +0100

    Improvements to the request forgery detection

---
+++ core/banner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+#coding: utf-8
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+#Author: the-Infected-Drake (@_tID)
+#This module requires XSRF-Probe
+#https://github.com/the-Infected-Drake/XSRF-Probe
+
+from core.colors import *
+import time
+
+def banner():
+
+	print '\n\n'
+	time.sleep(0.1)
+	print color.ORANGE+'     _____       _____       _____      _____       _____                                    '
+	time.sleep(0.1)
+	print color.RED+'  __'+color.ORANGE+'|'+color.RED+'__ '+color.ORANGE+'  |_  '+color.RED+'__'+color.ORANGE+'|'+color.RED+'___ '+color.ORANGE+' |_  '+color.RED+'__'+color.ORANGE+'|'+color.RED+'___  '+color.ORANGE+'|_  '+color.RED+'_'+color.ORANGE+'|'+color.RED+'____ '+color.ORANGE+'|_'+color.RED+'   _'+color.ORANGE+'|'+color.RED+'____ '+color.ORANGE+'|_ '+color.RED+' _____   _____  ______  ______  '
+	time.sleep(0.1)
+	print color.RED+" \  `  /    "+color.ORANGE+'|'+color.RED+'|   ___|   '+color.ORANGE+'|'+color.RED+'|  _  _|   '+color.ORANGE+'|'+color.RED+'|   ___|  '+color.ORANGE+'| '+color.RED+'|   _  |  '+color.ORANGE+"|"+color.RED+"|  _ ,' /     \|  _   )|   ___| "
+	time.sleep(0.1)
+	print color.RED+'  >   <     '+color.ORANGE+'|'+color.RED+' `-.`-.    '+color.ORANGE+'|'+color.RED+'|     \    '+color.ORANGE+'|'+color.RED+'|   ___|  '+color.ORANGE+'|'+color.RED+' |    __|  '+color.ORANGE+'|'+color.RED+'|     \ |  -  || |_  { |   ___| '
+	time.sleep(0.1)
+	print color.RED+' /__/__\   '+color.ORANGE+'_|'+color.RED+'|______|  '+color.ORANGE+'_|'+color.RED+'|__|\__\ '+color.ORANGE+' _|'+color.RED+'|___|   '+color.ORANGE+' _|'+color.RED+' |___|   '+color.ORANGE+' _|'+color.RED+'|__|\__\＼____/|______)|______| '
+	time.sleep(0.1)
+	print color.ORANGE+'    |_____|     |_____|     |_____|    |_____|     |_____| \n\n'
+	time.sleep(0.1)
+
