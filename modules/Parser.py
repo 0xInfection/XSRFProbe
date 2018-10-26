e26b1714a82f1596d67bc77e9a8ee7cc54fe8869
theinfecteddrake <theinfecteddrake@gmail.com>
2018-10-26 14:11:30 +0100

    Added improvements to parser module

---
+++ modules/Parser.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-:-:-:#
+#    XSRFProbe     #
+#-:-:-:-:-:-:-:-:-:#
+
+#Author: @_tID
+#This module requires XSRFProbe
+#https://github.com/theInfectedDrake/XSRFProbe
+
+import re
+from urllib.parse import urlsplit
+from core.colors import * # import ends
+
+def buildUrl(url, href): # receive form input type / url
+
+    exclusions = 'logout action=out action=logoff action=delete UserLogout osCsid file_manager.php'
+    if href == "http://localhost" or any((re.search(s,href)) for s in exclusions.split()):
+        return '' # csrf stuff :o
+
+    url_parts = urlsplit(url) # SplitResult(scheme, netloc, path, query, fragment)
+    href_parts = urlsplit(href)
+    app = '' # init to url storage
+
+    if href_parts.netloc == url_parts.netloc:
+        app = href # assuming this url is built
+
+    else:
+        if len(href_parts.netloc) == 0 and (len(href_parts.path) != 0 or len(href_parts.query) != 0): # parse result
+            domain = url_parts.netloc # done!
+            if href_parts.path.startswith('/'):
+                app = 'http://' + domain + href_parts.path # startpage dom
+            else:
+                try:
+                    app = 'http://' + domain + re.findall('(.*\/)[^\/]*', url_parts.path)[0] + href_parts.path
+                    # get real protocol urls
+                except IndexError:
+                    app = 'http://' + domain + href_parts.path
+            if href_parts.query:
+                app += '?' + href_parts.query # parameters :D
+
+    return app
+
+def buildAction(url, action):
+
+    print(O+'Parsing URL parameters...')
+    if action and not re.match('#', action): # make sure it is not a fragment (eg. http://site.tld/index.php#search)
+        return buildUrl(url, action) # get the url and reutrn it!
+    else:
+        return url # return the url itself if buildAction didn't identify the action
