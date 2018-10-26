7d1d7848df6717a69c8c81d1cc344e76d9681cb6
theinfecteddrake <theinfecteddrake@gmail.com>
2018-10-26 14:14:17 +0100

    Added new verbose mode base debugger

---
+++ core/verbout.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: @_tID
+# This module requires XSRFProbe
+# https://github.com/theInfectedDrake/XSRFProbe
+
+from core.colors import *
+from files.config import DEBUG as verbose
+
+def verbout(stat,content_info):
+    '''
+    This module is for giving a verbose
+                output.
+    '''
+    
+    # If debug mode is chosen as True
+    if verbose: 
+        # Concatenate the stat type and string value and print out
+        print(stat+content_info)
