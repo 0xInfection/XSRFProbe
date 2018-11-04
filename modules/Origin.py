669e3866e2acb8d25459e9b96e692f54cb5d56e3
0xInfection <theinfecteddrake@gmail.com>
2018-11-04 04:37:58 +0000

    Added origin checks

---
+++ modules/Origin.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Probe     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: @_tID
+# This module requires XSRFProbe
+# https://github.com/0xInfection/XSRFProbe
+
+import requests
+from core.colors import *
+from files.config import *
+from core.verbout import verbout
+from core.request import norm_request
+from core.randua import RandomAgent
+
+def Origin(url):
+    """
+    Check if the remote web application verifies the Origin before
+                    processing the HTTP request.
+    """
+
+    # Make the request normally and get content
+    verbout(O,'Making request on normal basis...')
+    req0x01 = norm_requester(url)
+    
+    # Set a fake Origin along with UA (pretending to be a 
+    # legitimate request from a browser)
+    verbout(GR,'Setting generic headers...')
+    gen_headers = HEADER_VALUES
+    gen_headers['Origin'] = ORIGIN_URL
+            
+    # We put the cookie in request, if cookie supplied :D
+    if COOKIE_VALUE:
+        gen_headers['Cookie'] = COOKIE_VALUE
+    
+    # Make the request with different Origin header and get the content
+    verbout(O,'Making request with tampered headers...')
+    req0x02 = norm_requester(url, headers=gen_headers)
+    
+    # Comparing the length of the requests' responses. If both content 
+    # lengths are same, then the site actually does not validate Origin 
+    # before processing the HTTP request which makes the site more 
+    # vulnerable to CSRF attacks.
+    #
+    # IMPORTANT NOTE: I'm aware that checking for the Origin header does
+    # NOT protect the application against all cases of CSRF, but it's a
+    # very good first step. In order to exploit a CSRF in an application
+    # that protects using this method an intruder would have to identify
+    # other vulnerabilities, such as XSS or open redirects, in the same
+    # domain.
+    #
+    # TODO: This algorithm has lots of room for improvement
+    if len(req0x01.content) != len(req0x02.content):
+        print(color.GREEN+' [+] Endoint '+color.ORANGE+'Origin Validation'+color.GREEN+' Present!')
+        print(color.GREEN+' [-] Heuristics reveal endpoint might NOT be vulnerable...')
+        return True
+    else:
+        print(color.RED+' [+] Endpoint '+color.ORANGE+'Origin Validation'+color.RED+' Not Present!')
+        print(color.RED+' [-] Heuristics reveal endpoint might be '+color.BR+' VULNERABLE '+color.RED+' to Origin Based CSRFs...')
+        print(color.GREEN+ ' [+] Possible CSRF Vulnerability Detected : '+color.ORANGE+url+'!')
+        print(color.ORANGE+' [!] Possible Vulnerability Type: '+color.BR+' Origin Based Request Forgery '+color.END)
+        return False
