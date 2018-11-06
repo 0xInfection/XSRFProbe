87a27b389fd32b8544a30e011b1214c78760a051
0xInfection <theinfecteddrake@gmail.com>
2018-11-06 08:45:54 +0000

    Added some changes to updater module

--- files/updater.py
+++ files/updater.py
@@ -16,13 +16,14 @@ from requests import get
 def updater():
     print(GR+'Checking for updates...')
     vno = get('https://raw.githubusercontent.com/0xInfection/XSRFProbe/master/files/VersionNum').text
+    print(GR+'Version on GitHub: '+color.CYAN+vno.strip())
+    print(GR+'Version You Have : '+color.CYAN+open('files/VersionNum').read())
     if vno != open('files/VersionNum').read():
         print(G+'A new version of XSRFProbe is available!')
         current_path = os.getcwd().split('/') # if you know it, you know it
         folder = current_path[-1] # current directory name
         path = '/'.join(current_path) # current directory path
         choice = input(O+'Would you like to update? [Y/n] :> ').lower()
-
         if choice != 'n':
             print(GR+'Updating XSRFProbe...')
             os.system('git clone --quiet https://github.com/0xInfection/XSRFProbe %s' % (folder))
