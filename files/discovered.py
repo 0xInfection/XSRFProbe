4b01eb94f1af6dd4d474015702e8096d3b675cd2
0xInfection <theinfecteddrake@gmail.com>
2018-11-07 08:44:36 +0000

    Added discovered.py for recording items

---
+++ files/discovered.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-:-:-:#
+#    XSRFProbe     #
+#-:-:-:-:-:-:-:-:-:#
+
+# Author: @_tID
+# This module requires XSRFProbe
+# https://github.com/0xInfection/XSRFProbe
+
+# INFO: This file is for storing the various important parts of
+# requests discovered during making of various requests.
+
+# This is for storing the various tokens which got discovered
+# during making the requests. This will be used for various
+# analysis of token generation prototypes and logic used in
+# generating them.
+REQUEST_TOKENS = []
+
+# List of all Urls that we found
+INTERNAL_URLS = []
