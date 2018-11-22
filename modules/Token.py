5b715763e1f9b47a8c3e50441b171c91c993e814
Infected Drake <theinfecteddrake@gmail.com>
2018-11-23 00:53:34 +0530

    Removed some uneasy code from Token.py

--- modules/Token.py
+++ modules/Token.py
@@ -45,7 +45,7 @@ def Token(req):
             verbout(R,'Request Parsing Execption!')
             verbout(R,'Error: '+e.__str__())
 
-        if param != '':
+        if param:
             return query, param
         else:
             verbout(color.RED,' [-] The form was requested '+color.BR+' Without an Anti-CSRF Token '+color.END+color.RED+'...')
