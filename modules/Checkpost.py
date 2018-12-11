b1dcf18924ec9cc58d8605c72aa84a84d000dc8d
0xInfection <theinfecteddrake@gmail.com>
2018-12-11 11:04:11 +0000

    Added changes in POST Based module

--- modules/Checkpost.py
+++ modules/Checkpost.py
@@ -15,8 +15,14 @@ import difflib
 from core.colors import *
 from core.verbout import verbout
 from urllib.parse import urlencode
+from modules.Generator import GeneratePoC
 
-def PostBased(url, r1, r2, r3, m_action, result, m_name=''):
+def PostBased(url, r1, r2, r3, m_action, result, genpoc, m_name=''):
+    '''
+    This method is for detecting POST-Based Request Forgeries
+        on basis of fuzzy string matching and comparison
+            based on Radcliff-Obershelp Algorithm.
+    '''
     checkdiffx1 = difflib.ndiff(r1.splitlines(1),r2.splitlines(1))  # check the diff noted
     checkdiffx2 = difflib.ndiff(r1.splitlines(1),r3.splitlines(1))  # check the diff noted
     result12 = []  # an init
@@ -32,25 +38,28 @@ def PostBased(url, r1, r2, r3, m_action, result, m_name=''):
     # If the number of differences of result12 are less than the number of differences
     # than result13 then we have the vulnerability. (very basic check)
     #
-    # NOTE: The alogrithm has lots of scopes of improvements
+    # NOTE: The algorithm has lots of scopes of improvement...
     if len(result12)<=len(result13):
         print(color.GREEN+ ' [+] CSRF Vulnerability Detected : '+color.ORANGE+url+'!')
         print(color.ORANGE+' [!] Vulnerability Type: '+color.BR+' POST-Based Request Forgery '+color.END)
         time.sleep(0.3)
-        verbout(O,'PoC of response and request...')
+        verbout(O, 'PoC of response and request...')
         if m_name:
-            print(color.RED+'\n +---------+')
-            print(color.RED+' |   PoC   |')
-            print(color.RED+' +---------+\n')
+            print(color.RED+'\n +-----------------+')
+            print(color.RED+' |   Request PoC   |')
+            print(color.RED+' +-----------------+\n')
             print(color.BLUE+' [+] URL : ' +color.CYAN+url)  # url part
             print(color.CYAN+' [+] Name : ' +color.ORANGE+m_name)  # name
-            print(color.GREEN+' [+] Action : ' +color.END+m_action)  # action
+            print(color.GREEN+' [+] Action : ' +color.END+'/'+m_action.rsplit('/', 1)[1])  # action
         else:  # if value m['name'] not there :(
-            print(color.RED+'\n +---------+')
-            print(color.RED+' |   PoC   |')
-            print(color.RED+' +---------+\n')
+            print(color.RED+'\n +-----------------+')
+            print(color.RED+' |   Request PoC   |')
+            print(color.RED+' +-----------------+\n')
             print(color.BLUE+' [+] URL : ' +color.CYAN+url)  # the url
-            print(color.GREEN+' [+] Action : ' +color.END+ m_action)  # action
-
+            print(color.GREEN+' [+] Action : ' +color.END+'/'+m_action.rsplit('/', 1)[1])  # action
         print(color.ORANGE+' [+] Query : '+color.GREY+ urlencode(result).strip())
-        print('')  # print out the params + url
+        print(GR, 'Generating PoC Form...' )
+        print(color.RED+'\n +--------------+')
+        print(color.RED+' |   Form PoC   |')
+        print(color.RED+' +--------------+\n'+color.CYAN)
+        GeneratePoC(url, str(genpoc))
