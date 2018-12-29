3c0d1753746c9a14165022c879b027db43099418
0xInfection <theinfecteddrake@gmail.com>
2018-12-29 16:17:17 +0000

    Other necessary changes to code infrastructure

--- core/forms.py
+++ core/forms.py
@@ -11,7 +11,7 @@
 
 def testFormx1(): # an example xsrfprobe-test-form to make sure the stuff works properly ;)
 
-    test_form_0x01 = """<xsrfprobe-test-form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
+    test_form_0x01 = """<form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
     <div><div class="xsrfprobe-test-form-item" id="edit-name-wrapper">
      <label for="edit-name">Username: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
      <input type="text" maxlength="60" name="name" id="edit-name" size="15" value="test1" class="xsrfprobe-test-form-text required" />
@@ -25,13 +25,13 @@ def testFormx1(): # an example xsrfprobe-test-form to make sure the stuff works 
     <li class="last"><a href="/somendpoint/tho" title="Request new password via e-mail.">Request new password</a></li>
     </ul></div><input type="hidden" name="xsrfprobe-test-form_build_id" id="xsrfprobe-test-form-6ab908" value="xsrfprobe-test-form-6a060cc6cb908"  />
     <input type="hidden" name="xsrfprobe-test-form_id" id="edit-xsrfprobe-block" value="user_login_block"  />
-    </div></xsrfprobe-test-form> """
+    </div></form> """
 
     return test_form_0x01
 
 def testFormx2(): # an example of a xsrfprobe-test-form (used drupal)
 
-    test_form_0x02 = """<xsrfprobe-test-form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
+    test_form_0x02 = """<form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
     <div><div class="xsrfprobe-test-form-item" id="edit-name-wrapper">
      <label for="edit-name">Username: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
      <input type="text" maxlength="60" name="name" id="edit-name" size="15" value="test2" class="xsrfprobe-test-form-text required" />
@@ -45,6 +45,6 @@ def testFormx2(): # an example of a xsrfprobe-test-form (used drupal)
     <li class="last"><a href="/somendpoint/tho" title="Request new password via e-mail.">Request new password</a></li>
     </ul></div><input type="hidden" name="xsrfprobe-test-form_build_id" id="xsrfprobe-test-form-6a060cc6cb908" value="xsrfprobe-test-form-6a060cc6cb908"  />
     <input type="hidden" name="xsrfprobe-test-form_id" id="edit-xsrfprobe-block" value="user_login_block"  />
-    </div></xsrfprobe-test-form> """
+    </div></form> """
 
     return test_form_0x02
