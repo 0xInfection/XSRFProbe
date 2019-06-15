6270eaa1d62fcaa873685edfa39a2e404320a30f
0xInfection <theinfecteddrake@gmail.com>
2019-06-15 10:50:14 +0530

    Updated forms to prevent some nibbas claiming CoPyRiGhT

--- core/forms.py
+++ core/forms.py
@@ -1,50 +1,60 @@
 #!/usr/bin/env python3
-#coding: utf-8
+# coding: utf-8
 
 #-:-:-:-:-:-:-::-:-:#
 #    XSRF Probe     #
 #-:-:-:-:-:-:-::-:-:#
 
-#Author: 0xInfection (@_tID)
-#This module requires XSRF-Probe
-#https://github.com/0xInfection/XSRF-Probe
+# Author: 0xInfection
+# This module requires XSRFProbe
+# https://github.com/0xInfection/XSRFProbe
 
 def testFormx1(): # an example xsrfprobe-test-form to make sure the stuff works properly ;)
 
-    test_form_0x01 = """<form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
-    <div><div class="xsrfprobe-test-form-item" id="edit-name-wrapper">
-     <label for="edit-name">Username: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
-     <input type="text" maxlength="60" name="name" id="edit-name" size="15" value="test1" class="xsrfprobe-test-form-text required" />
-    </div>
-    <div class="xsrfprobe-test-form-item" id="edit-pass-wrapper">
-     <label for="edit-pass">Password: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
-     <input type="password" value="a9z8e7" name="pass" id="edit-pass"  maxlength="60"  class="xsrfprobe-test-form-text required" />
-    </div>
-    <input type="submit" name="op" id="edit-submit" value="Log in"  class="xsrfprobe-test-form-submit" />
-    <div class="item-list"><ul><li class="first"><a href="/somednpoint/register" title="Create a new user account.">Create new account</a></li>
-    <li class="last"><a href="/somendpoint/tho" title="Request new password via e-mail.">Request new password</a></li>
-    </ul></div><input type="hidden" name="xsrfprobe-test-form_build_id" id="xsrfprobe-test-form-6ab908" value="xsrfprobe-test-form-6a060cc6cb908"  />
-    <input type="hidden" name="xsrfprobe-test-form_id" id="edit-xsrfprobe-block" value="user_login_block"  />
-    </div></form> """
+    test_form_0x01 = """
+    <form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br>
+		<input name="log" class="input" id="user_login" type="text" size="20" value="" autocapitalize="off"></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br>
+		<input name="pwd" class="input" id="user_pass" type="password" size="20" value=""></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" value="forever"> Remember Me</label></p>
+	<p class="submit">
+		<input name="wp-submit" class="button button-primary button-large" id="wp-submit" type="submit" value="Log In">
+				<input name="redirect_to" type="hidden" value="/niqqa.php">
+					<input name="testcookie" type="hidden" value="1">
+	</p>
+	</form> """
 
     return test_form_0x01
 
 def testFormx2(): # an example of a xsrfprobe-test-form (used drupal)
 
-    test_form_0x02 = """<form action="/somendpoint" method="post" id="xsrfprobe-xsrfprobe-test-form">
-    <div><div class="xsrfprobe-test-form-item" id="edit-name-wrapper">
-     <label for="edit-name">Username: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
-     <input type="text" maxlength="60" name="name" id="edit-name" size="15" value="test2" class="xsrfprobe-test-form-text required" />
-    </div>
-    <div class="xsrfprobe-test-form-item" id="edit-pass-wrapper">
-     <label for="edit-pass">Password: <span class="xsrfprobe-test-form-required" title="This field is required.">*</span></label>
-     <input type="password" value="a9z8e7" name="pass" id="edit-pass"  maxlength="60"  size="15"  class="xsrfprobe-test-form-text required" />
-    </div>
-    <input type="submit" name="op" id="edit-submit" value="Log in"  class="xsrfprobe-test-form-submit" />
-    <div class="item-list"><ul><li class="first"><a href="/somednpoint/register" title="Create a new user account.">Create new account</a></li>
-    <li class="last"><a href="/somendpoint/tho" title="Request new password via e-mail.">Request new password</a></li>
-    </ul></div><input type="hidden" name="xsrfprobe-test-form_build_id" id="xsrfprobe-test-form-6a060cc6cb908" value="xsrfprobe-test-form-6a060cc6cb908"  />
-    <input type="hidden" name="xsrfprobe-test-form_id" id="edit-xsrfprobe-block" value="user_login_block"  />
-    </div></form> """
+    test_form_0x02 = """
+    <form name='shit' id="contact" action="/nibba.php" method="post">
+     <h3>Colorlib Contact Form</h3>
+     <h4>Contact us for custom quote</h4>
+     <fieldset>
+     <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+     </fieldset>
+     <fieldset>
+     <input placeholder="Your Email Address" type="email" tabindex="2" required>
+     </fieldset>
+     <fieldset>
+     <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required>
+     </fieldset>
+     <fieldset>
+     <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
+     </fieldset>
+     <fieldset>
+     <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+     </fieldset>
+     <fieldset>
+     <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+     </fieldset>
+    </form> """
 
     return test_form_0x02
