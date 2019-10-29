1ac2cc1e62dcf8746acfa87f974d77bcb8cd199f
0xInfection <theinfecteddrake@gmail.com>
2019-10-29 20:15:14 +0530

    Update

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-#-:-:-:-:-:-:-::-:-:#
-#    XSRF Probe     #
-#-:-:-:-:-:-:-::-:-:#
-
-#This module requires XSRFProbe
-#https://github.com/0xInfection/XSRFProbe
