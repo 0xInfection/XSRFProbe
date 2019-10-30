027b0bb7e98c753c5e1c126eb2bb5041352f01d1
0xInfection <theinfecteddrake@gmail.com>
2019-10-30 13:02:57 +0530

    Added minor error fix

--- xsrfprobe/core/options.py
+++ xsrfprobe/core/options.py
@@ -67,7 +67,7 @@ if args.update:
 
 # Print out XSRFProbe version
 if args.version:
-    print('\n\033[1;97m [+] \033[1;91mXSRFProbe Version\033[0m : \033[1;97m'+__version__+'\033[0m'
+    print('\n\033[1;97m [+] \033[1;91mXSRFProbe Version\033[0m : \033[1;97m'+__version__+'\033[0m\n')
     quit()
 
 # Now lets update some global config variables
