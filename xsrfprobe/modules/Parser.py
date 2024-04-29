a07fd88e5173ae346990d29170363f017ebb3b12
Noam Rathaus <rathaus@gmail.com>
2024-04-29 15:52:12 +0300

    Use URL parts to build the new URL

--- xsrfprobe/modules/Parser.py
+++ xsrfprobe/modules/Parser.py
@@ -43,29 +43,33 @@ def buildUrl(url, href):  # receive form input type / url
     # If Url and Destination have the same domain...
     if href_parts.netloc == url_parts.netloc:
         app = href  # Assigning the main netloc
+        return app
 
-    else:  # If the destination Url doesn't have a domain
-        if len(href_parts.netloc) == 0 and (
-            len(href_parts.path) != 0 or len(href_parts.query) != 0
-        ):
-            domain = url_parts.netloc  # Assigning the main domain
-            if href_parts.path.startswith(
-                "/"
-            ):  # If the href starts with a '/', it is an internal Url
-                app = url_parts.scheme + "://" + domain + href_parts.path  # Startpage
-            else:
-                try:
-                    app = (
-                        "http://"
-                        + domain
-                        + re.findall(PROTOCOLS, url_parts.path)[0]
-                        + href_parts.path
-                    )
-                    # Get real protocol urls
-                except IndexError:
-                    app = "http://" + domain + href_parts.path
-            if href_parts.query:  # Checking if any queries were there...
-                app += "?" + href_parts.query  # Adding the query paramaters to Url
+    # if netloc of href_parts is empty, but we have a path or query
+    #  build it from scratch
+    if href_parts.netloc == "" and (href_parts.path != "" or href_parts.query != ""):
+        domain = url_parts.hostname  # Assigning the main domain
+        if href_parts.path.startswith("/"):
+            # If the href starts with a '/', it is an internal Url
+            app = f"{url_parts.scheme}://{domain}"
+            if url_parts.port is not None:
+                app += f":{url_parts.port}"
+
+            app += f"{href_parts.path}"  # Startpage
+        else:
+            try:
+                app = f"{url_parts.scheme}://{domain}"
+                if url_parts.port is not None:
+                    app += f":{url_parts.port}"
+
+                app += re.findall(PROTOCOLS, url_parts.path)[0] + href_parts.path
+                # Get real protocol urls
+            except IndexError:
+                app = "http://" + domain + href_parts.path
+
+        if href_parts.query:  # Checking if any queries were there...
+            app += "?" + href_parts.query  # Adding the query parameters to Url
+
     # Return '' for invalid url, url otherwise
     return app
 
